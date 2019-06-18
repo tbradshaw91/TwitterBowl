bab6d30cfcc7a67694e3672f30d48fa81f9abaa9
Patricia Bradshaw <46237708+tbradshaw91@users.noreply.github.com>
2019-06-17 19:38:15 -0700

    Adding files from today

---
+++ __init__.py
@@ -0,0 +1,4 @@
+"""Entry point for TwitterBowl Flask application."""
+from .app import create_app
+
+APP = create_app()
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .models import DB
+
+def create_app():
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    DB.init_app(app)
+
+    @app.route('/')
+    def root():
+        return 'Hello TwitOff!'
+
+    return app
+
